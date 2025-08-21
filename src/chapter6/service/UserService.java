3bd5609626b0e95de285f6e5436631132e47845e
sato_issei <sato.issei@alhinc.jp>
2025-08-21 15:55:16 +0900

    簡易Twitter実践問題① レビュー後修正

--- src/chapter6/service/UserService.java
+++ src/chapter6/service/UserService.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.commons.lang.StringUtils;
+
 import chapter6.beans.User;
 import chapter6.dao.UserDao;
 import chapter6.logging.InitApplication;
@@ -121,7 +123,7 @@ public class UserService {
 
 		Connection connection = null;
 		try {
-			if(!user.getPassword().isEmpty()) {
+			if(!StringUtils.isBlank(user.getPassword())) {
 				// パスワード暗号化
 				String encPassword = CipherUtil.encrypt(user.getPassword());
 				user.setPassword(encPassword);
