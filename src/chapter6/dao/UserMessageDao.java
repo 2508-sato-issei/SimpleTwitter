562f68aed472f097e33b74f66a0e574973f5f6cb
sato_issei <sato.issei@alhinc.jp>
2025-08-22 09:29:25 +0900

    各ユーザー毎のつぶやきのみ表示する機能を実装

--- src/chapter6/dao/UserMessageDao.java
+++ src/chapter6/dao/UserMessageDao.java
@@ -31,7 +31,7 @@ public class UserMessageDao {
 		applicaton.init();
 	}
 
-	public List<UserMessage> select(Connection connection, int num) {
+	public List<UserMessage> select(Connection connection, Integer id, int num) {
 
 		log.info(new Object(){}.getClass().getEnclosingClass().getName() +
 				" : " + new Object(){}.getClass().getEnclosingMethod().getName());
@@ -49,9 +49,16 @@ public class UserMessageDao {
 			sql.append("FROM messages ");
 			sql.append("INNER JOIN users ");
 			sql.append("ON messages.user_id = users.id ");
+			if(id != null) {
+				sql.append("WHERE messages.user_id = ? ");
+			}
 			sql.append("ORDER BY created_date DESC limit " + num);
 
 			ps = connection.prepareStatement(sql.toString());
+
+			if(id != null) {
+				ps.setInt(1, id);
+			}
 
 			ResultSet rs = ps.executeQuery();
 
